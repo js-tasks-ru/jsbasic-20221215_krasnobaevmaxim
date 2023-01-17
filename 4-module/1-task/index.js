767ad48c5501c30efab918ac59f44f1a0afcb225
Maxim Krasnobaev <toyscrew1@gmail.com>
2023-01-17 10:24:59 +0300

    Решения задач модуля 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  let li = document.createElement('li');
+
+  for (key of friends) {
+    li.innerHTML('beforeEnd', `<li>${key.firstName} + ${key.lastName}</li>`);
+  }
+
+  ul.append(li);
+  return ul;
 }
+
